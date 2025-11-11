ed1ab67bc8b50f0b9aaf925e4f74b210284d2b9b
arafat-alim-office <arafat@perpetuitycapital.co.in>
2025-11-11 17:22:57 +0530

    ✅ Handled 404 route.

--- index.js
+++ index.js
@@ -60,12 +60,12 @@ app.use((err, req, res, next) => {
   });
 });
 
-//! 404 handler
-// app.use("*", (req, res) => {
-//   res.status(404).json({
-//     error: "Route not found",
-//   });
-// });
+//! 404 Not Found Handler (must be the last middleware)
+app.use((req, res, next) => {
+  res.status(404).json({
+    error: "Sorry, couldn't find that page!",
+  });
+});
 
 //! start server
 app.listen(PORT, () => {
